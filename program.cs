6899cbd46133b5ed8feaaa909e200bfd04dd3938
DeezFurry <105929248+DeezFurry@users.noreply.github.com>
2022-06-13 14:18:57 +0200

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,1 +1,14 @@
-using 
+using System;
+
+namespace Console Program
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("This is my first Console Program")
+      Console.WriteLine("Thanks for testing it")
+    }
+  }
+}
